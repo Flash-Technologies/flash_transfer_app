a9c3434408c5cca84267a57f219df787c3c7208c
shaikhasif69 <theshaikhasif03@gmail.com>
2025-09-20 23:49:34 +0530

    Update Kotlin and Gradle configurations for improved build performance
    - Upgraded Kotlin Gradle plugin from 1.9.22 to 2.1.0 in build.gradle.kts.
    - Increased JVM memory allocation from 2048m to 4096m in gradle.properties.
    - Updated Kotlin Android plugin version from 1.8.22 to 2.1.0 in settings.gradle.kts.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.4")   // Updated from 7.4.2
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22") 
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0") 
         classpath("com.google.gms:google-services:4.4.3")  // Add Firebase Google Services plugin
     }
 }
